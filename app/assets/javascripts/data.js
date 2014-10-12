5ba4601c126b9049627c4336e2635e67e0a0d083
Spencer Zhang <syz4@cornell.edu>
2014-10-12 03:52:29 -0500

    Added getData interval in javascript

--- app/assets/javascripts/data.js
+++ app/assets/javascripts/data.js
@@ -1,14 +1,28 @@
 var measurements = [];
+var delay = 0;
 
 function getData() {
+  console.log("test")
   $.get('/data', function(data) {
-    console.log(data);
-    measurements.push(data);
+    if(data['created_at'] != measurements[measurements.length-1]['created_at']) {
+      measurements.push(data);
+      delay = 0;
+    } else {
+      delay += 1;
+      if(delay >= 2) {
+        measurements.push(undefined)
+        delay = 0;
+      }
+    }
   })
+
+  $('#test').text(measurements);
 }
 
 $(function() {
   var svg = d3.select('#canvas').append('svg')
 
-  window.getData = getData
+  setInterval(function() {
+    getData()
+  }, 500);
 })
