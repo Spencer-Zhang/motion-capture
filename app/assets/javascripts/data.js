4dda7b4b4157aaa55db8cb7cbbda434ef65ac29f
Spencer Zhang <syz4@cornell.edu>
2014-10-12 04:07:30 -0500

    Added getDelay function

--- app/assets/javascripts/data.js
+++ app/assets/javascripts/data.js
@@ -6,6 +6,22 @@ function getData() {
   })
 
   $('#test').text(measurements[measurements.length - 1]['created_at']);
+  $('#delay').text(getDelay());
+}
+
+function getDelay() {
+  if(measurements.length == 0) {
+    return 0;
+  } else {
+    var index = measurements.length - 1;
+    var latestTime = measurements[index]['created_at'];
+    index -= 1
+    while(index >= 0) {
+      if(measurements[index]['created_at'] != latestTime) { break }
+      index -= 1;
+    }
+    return measurements.length - 1 - index;
+  }
 }
 
 $(function() {
