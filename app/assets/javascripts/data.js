b101b3bc7a22b20883d1886731cc3defd70e0511
Spencer Zhang <syz4@cornell.edu>
2014-10-12 05:20:18 -0500

    Implemented d3 graph

--- app/assets/javascripts/data.js
+++ app/assets/javascripts/data.js
@@ -1,17 +1,27 @@
 var measurements = [];
+var temperatures = [];
 
 function getData() {
   $.get('/data', function(data) {
     measurements.push(data);
-  })
+    if(getDelay() > 4) {
+      temperatures.push(20)
+    } else {
+      temperatures.push(data['temperature'])
+    }
 
-  $('#test').text(measurements[measurements.length - 1]['created_at']);
-  $('#delay').text(getDelay());
+    if(temperatures.length > 50) {
+      temperatures.splice(0, 1)
+    }
+
+    updateStatus();
+    drawLines();
+  })
 }
 
 function getDelay() {
   if(measurements.length == 0) {
-    return 0;
+    return 99;
   } else {
     var index = measurements.length - 1;
     var latestTime = measurements[index]['created_at'];
@@ -24,8 +34,69 @@ function getDelay() {
   }
 }
 
+function updateStatus() {
+  if(getDelay() > 5) {
+    $('#status').text('OK').css('background-color', 'green')
+  } else {
+    $('#status').text(temperatures[temperatures.length-1] + "\xb0C")
+      .css('background-color', 'red')
+  }
+}
+
+var y = d3.scale.linear().domain([20,0]).range([50, 250])
+var x = d3.scale.linear().domain([-50, 0]).range([50, 750])
+
+function drawLines() {
+  d3.select('#lines').selectAll('path').remove()
+
+  var line = d3.svg.line()
+    .interpolate('cardinal')
+    .x(function(d,i) { return x(i - temperatures.length) })
+    .y(function(d) { return y(d) })
+
+  d3.select('#lines').append("svg:path").attr('d', line(temperatures))
+    .attr("stroke", "black")
+    .attr('fill', 'none')
+}
+
 $(function() {
   var svg = d3.select('#canvas').append('svg')
+    .attr('width', 800)
+    .attr('height', 300)
+
+  var lines = svg.append('g')
+    .attr('id', 'lines')
+
+  lines.append("svg:line")
+    .attr("x1", x(-50))
+    .attr("y1", y(0))
+    .attr("x2", x(0))
+    .attr("y2", y(0))
+    .attr('stroke', 'black')
+
+  lines.selectAll("horizLines")
+    .data(y.ticks(4)).enter()
+      .append("svg:line")
+      .attr("x1", x(-50))
+      .attr("y1", function(d) {return y(d)})
+      .attr("x2", x(0))
+      .attr("y2", function(d) {return y(d)})
+      .attr('stroke', '#cccccc')
+
+  lines.selectAll("horizLabel")
+    .data(y.ticks(4)).enter()
+      .append("svg:text")
+      .text(String)
+      .attr('x', x(1)-3)
+      .attr('y', function(d) {return y(d)+5})
+ 
+  lines.append("svg:line")
+    .attr("x1", x(0))
+    .attr("y1", y(0))
+    .attr("x2", x(0))
+    .attr("y2", y(20))
+    .attr('stroke', 'black')
+
 
   setInterval(function() {
     getData()
